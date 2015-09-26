b872882c8f6dd85c50d858c0b99098a4d9af7f18
Antonello <lelloxd@gmail.com>
2015-09-26 15:49:32 +0200

    test
    test 2

--- back-end/login.php
+++ back-end/login.php
@@ -1,24 +1,10 @@
 <?php
 //avvio la sessione
 session_start();
-
-// recupero i valori passati via POST
-$username = trim($_POST["username"]);
-$password = trim($_POST["password"]);
-/*if (get_magic_quotes_gpc()){
-		$username = stripslashes($username);
-		$password = stripslashes($password);
-}*/
-
-$username = mysqli_real_escape_string($conn, $username;
-$password = mysqli_real_escape_string($conn, $password);
-
-$password = md5($password);
-
 // mi connetto al MySQL definendo host, user, pwd e nome del db
 $db_host = "localhost";
 $db_user = "root";
-$db_pass = "root";
+$db_pass = "";
 $db_name = "simonemilan";
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
@@ -29,8 +15,26 @@ if (mysqli_connect_errno())
 }
 //parte della connessione da spostare in un file connect_db.php
 
+if (isset($_POST["username"]) && isset($_POST["password"])) {
+	// recupero i valori passati via POST
+	$username = $_POST["username"];
+	$password = $_POST["password"];
+	$username = mysqli_real_escape_string($conn, $username);
+	$password = mysqli_real_escape_string($conn, $password);
+
+	$password = md5($password);
+}
+else echo "schifolafiga";
+
+/*if (get_magic_quotes_gpc()){
+		$username = stripslashes($username);
+		$password = stripslashes($password);
+}*/
+
+
+
 //query per verificare la correttezza del login
-$query("SELECT * FROM admin_login WHERE username = '$username' AND password = '$password'");
+$query = "SELECT * FROM admin_login WHERE username = $username AND password = $password";
 $result = mysqli_query($conn, $query);
 //controllo che ci sia qualcosa dentro a $results
 if ($result)
@@ -45,7 +49,7 @@ if ($result)
     // se i dati sono sbagliati resetto la variabile di sessione e rimando al form di login
     $_SESSION['userid'] = "";
     session_destroy();
-    header('location: form_login.php'):
+    header('location:form_login.php');
   }
 }else{
   // se non ci sono risultati stampo zero
